9146a5fd0b60cddf2c7ae84c5e260ac1af10498d
Mate Antičević <mate@anticevic.net>
2020-04-28 00:17:20 +0200

    Remove api dependency

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,10 @@ type State = {
 
 class IndexPage extends React.Component<{}, State> {
 
-    connection: HubConnection;
+    connection: HubConnection = new HubConnectionBuilder()
+        .withUrl("https://hub.anticevic.net/TrackingHub")
+        .configureLogging(LogLevel.Information)
+        .build();
 
     state: State = {
         center: { lat: 0, lng: 0 },
@@ -23,24 +26,7 @@ class IndexPage extends React.Component<{}, State> {
         zoom: 3
     }
 
-    constructor() {
-        super();
-
-        this.connection = new HubConnectionBuilder()
-            .withUrl("https://hub.anticevic.net/TrackingHub")
-            .configureLogging(LogLevel.Information)
-            .build();
-    }
-
     componentDidMount() {
-        fetch("https://api.anticevic.net/tracking/latest?username=mate").then(x => x.json()).then(r => {
-            this.setState({
-                center: { lat: r.Latitude, lng: r.Longitude },
-                currentLocation: { latitude: r.Latitude, longitude: r.Longitude, speed: r.Speed },
-                zoom: 12
-            });
-        });
-
         this.connection.start();
 
         this.connection.on("Receive", tracking => {
@@ -54,16 +40,14 @@ class IndexPage extends React.Component<{}, State> {
                     longitude: tracking.longitude,
                     speed: tracking.speed
                 },
-                isMoving: true,
+                isMoving: false,
                 zoom: 15
             });
         });
     }
 
     render() {
-
         const { center, currentLocation, isMoving, zoom } = this.state;
-
         const labelText = isMoving ? `${Math.ceil(3.6 * currentLocation!.speed)} km/h` : ' ';
 
         return (
